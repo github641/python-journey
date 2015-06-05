fe1ede3c0759057848698f6f8b309f864a1b7db3
刘志意 <mrliuzy@outlook.com>
2015-06-05 10:41:21 +0800

    Create IpReverse.py
    ####创建一个从整数到IP 地址的转换程序,如下格式: WWW.XXX.YYY.ZZZ.

---
+++ IpReverse.py
@@ -0,0 +1,25 @@
+
+
+def tranFromIPToInt(strIP):
+    lst = strIP.split(".")
+    if len(lst) != 4:
+        return "error ip"
+    return int(lst[0]) * (256 ** 3) + int(lst[1]) * (256 ** 2) +\
+           int(lst[2]) * 256 + int(lst[3])
+def tranFromIntToIP(strInt):
+    ip1 = 0
+    ip2 = 0
+    ip3 = 0
+    ip4 = 0
+    ip1 = int(strInt) / (256 ** 3)
+    ip2 = (int(strInt) % (256 ** 3)) / (256 ** 2)
+    ip3 = (int(strInt) % (256 ** 2)) / 256
+    ip4 = int(strInt) % 256
+    return str(ip1) + "." + str(ip2) + "." + str(ip3) + "." + str(ip4)
+c=raw_input( "input 'b' for IP-->INT or 'a' for INT-->IP :")
+if c=='a':
+    x=raw_input('A int: ')
+    print tranFromIntToIP(x)
+elif c=='b':
+    y=raw_input('A IP: ')
+    print tranFromIPToInt(y)
