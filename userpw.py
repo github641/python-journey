c17a610211cb1104b6af0fde570d5800eef5fd3c
刘志意 <mrliuzy@outlook.com>
2015-06-05 10:22:22 +0800

    Update userpw.py
    使它能记录用户上次的登录日期和时间(用time 模块)，并与用户密码一起 保存起来。程序的界面有要求用户输入用户名和密码的提示。无论户名是否成功登录，都应有提示， 在户名成功登录后，应更新相应用户的上次登录时间戳。如果本次登录与上次登录在时间上相差不 超过4 个小时，则通知该用户： “You already logged in at: .”

--- userpw.py
+++ userpw.py
@@ -1,30 +1,42 @@
 #!/usr/bin/env python
 
+from time import time,ctime #导入模块
 db = {}
 
 def newuser():
     prompt = 'login desired: '
-    while 1:
+    while True:
         name = raw_input(prompt)
         if db.has_key(name):
             prompt = 'name taken, try another: '
             continue
         else:
             break
-    pwd = raw_input('passwd: ')
-    db[name] = pwd
+    pwd = raw_input('passwd: ')#用户名合格后，要求输入密码
+    logt=time()#获取时间戳
+    db[name]=[pwd,logt]#把密码和时间组成列表存到字典，共享一个键
 
-def olduser():
-    name = raw_input('login: ')
-    pwd = raw_input('passwd: ')
-    passwd = db.get(name)
-    if passwd == pwd:
-        pass
-    else:
-        print 'login incorrect'
-        return
+def olduser(): 
+    name=raw_input('login: ') 
+    pwd=raw_input('passwd: ')
+    if name in db:#检测是否是老用户
+        if pwd == db.get(name)[0]: #获取老用户密码与输入密码比较
+            print 'welcome back',name #显示欢迎信息
+            print 'You lasttime logged in at:',ctime(db[name][1])
+            #显示上次登录的时间戳
+            current=time()#获取当前时间
+            delta=current-db[name][1]#求上次登录与现在时间的时间差
+            if delta<=14400: #判断是否在4小时内
+                print 'You already logged in 4 hours period!'
 
-    print 'welcome back', name
+            else:#若时间差不在四小时之内
+                logt=time()#获取当前时间 
+                db[name][1]=logt #把密码和时间组成列表存到字典，共享一个键，更新时间戳
+        else: #密码错误
+            print 'login incorrect'  
+
+    else: #不是老用户
+        print 'login incorrect'  
 
 def showmenu():
     prompt = """
@@ -34,12 +46,12 @@ def showmenu():
 
 Enter choice: """
 
-    done = 0
+    done = False
     while not done:
-        chosen = 0
+        chosen = False
         while not chosen:
             try:
-                choice = raw_input(prompt)[0]
+                choice = raw_input(prompt).strip()[0].lower()
             except (EOFError, KeyboardInterrupt):
                 choice = 'q'
             print '\nYou picked: [%s]' % choice
@@ -47,7 +59,7 @@ Enter choice: """
             if choice not in 'neq':
                 print 'invalid menu option, try again'
             else:
-                chosen = 1
+                chosen = True
 
         if choice == 'q': done = 1
         if choice == 'n': newuser()
